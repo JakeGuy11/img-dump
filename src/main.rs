1e2f306c475fababed79dc95e3811a8682423c81
Jake <Jake_Guy_11@protonmail.ch>
2021-07-10 10:13:42 -0400

    fixed user entry alignment

--- src/main.rs
+++ src/main.rs
@@ -11,20 +11,33 @@ fn main()
         window.set_title("My actual window title");
         window.set_default_size(800, 600);
 
-        let container = Box::new(gtk::Orientation::Vertical, 10);
+        // Define all the containers
+        let entry_container = Box::new(gtk::Orientation::Horizontal, 10);
+        entry_container.set_valign(gtk::Align::Start);
+
         let label = Label::new(Some("This is text"));
-        let input_text = Entry::new();
-        let button = Button::with_label("I am a button");
+        let user_entry = Entry::new();
+        user_entry.set_valign(gtk::Align::Fill);
+        let exit_button = Button::with_label("Exit");
+        exit_button.set_valign(gtk::Align::Fill);
 
-        container.add(&label);
-        container.add(&input_text);
-        container.add(&button);
-        window.add(&container);
+        //base_container.add(&label);
+        entry_container.pack_start(&user_entry, true, true, 10);
+        entry_container.add(&exit_button);
+        //base_container.add(&entry_container);
 
-        input_text.connect_activate(move |entry_field|
+        //window.add(&label);
+        window.add(&entry_container);
+
+        user_entry.connect_activate(move |entry_field|
         {
-            let entered_text = &entry_field.text();
+            let entered_text = format! ("{}", &entry_field.alignment());
             &label.set_label(entered_text.as_str());
+        });
+
+        exit_button.connect_clicked(move |_|
+        {
+            std::process::exit(0);
         });
 
         window.show_all();
